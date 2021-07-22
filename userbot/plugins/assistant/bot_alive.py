350162856c119ac4395c1da29cd4fbd08986433a
angad005 <75972578+angad005@users.noreply.github.com>
2021-07-22 11:44:31 +0530

    Update bot_alive.py

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -3,20 +3,17 @@ from telethon import events
 from userbot import ALIVE_NAME, bot
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/22e70478bd48568451a1f.jpg"
+PM_IMG = "https://telegra.ph/file/5a7945894898e8cee4a26.jpg"
 pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
 pm_caption += "➥ **SYSTEMS STATS**\n"
-pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
 pm_caption += "➥ **Python:** `3.8.6` \n"
 pm_caption += "➥ **Database Status:**  `Functional`\n"
 pm_caption += "➥ **Current Branch** : `master`\n"
-pm_caption += f"➥ **Version** : `2.0`\n"
 pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
 pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/DARK-COBRA/DARKCOBRA/blob/master/LICENSE)\n"
-pm_caption += "➥ **Copyright** : By [DarkCobra](https://t.me/Dark_cobra_support_group)\n"
-pm_caption += "[Assistant By DarkCobra](https://t.me/Dark_cobra_support_group)"
-
+pm_caption += "➥ **support** : (https://t.me/tsf_userbot_support)\n"
+pm_caption += "[Assistant By TSF](https://t.me/tsf_userbot)"
+pm_caption += f"➥ **TSF on fire 🔥**\n"
 # only Owner Can Use it
 @tgbot.on(events.NewMessage(pattern="^/alive", func=lambda e: e.sender_id == bot.uid))
 async def _(event):
